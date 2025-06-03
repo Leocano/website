a3a54465ce8bb90a7fcd71f6dd80bcd62493ee7d
Leonardo Cano <leocano4242@gmail.com>
2025-06-02 23:39:27 -0300

    Add editor buttons

--- src/components/EditorDoodle.tsx
+++ src/components/EditorDoodle.tsx
@@ -1,7 +1,11 @@
 export function EditorDoodle() {
   return (
     <div className="m-auto mt-28 w-300">
-      <div className="h-10 w-full rounded-t-2xl bg-neutral-900"></div>
+      <div className="flex gap-2 rounded-t-2xl bg-neutral-900 p-2 text-right">
+        <span className="size-3 rounded-full bg-green-600" />
+        <span className="size-3 rounded-full bg-yellow-400" />
+        <span className="size-3 rounded-full bg-red-600" />
+      </div>
       <div className="h-150 w-full rounded-b-2xl bg-neutral-800" />
     </div>
   );
