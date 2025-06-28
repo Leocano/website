7f6ae39a2463a8b02797a633a98b3beb35ef7252
Leonardo Cano <leocano4242@gmail.com>
2025-06-28 00:14:40 -0300

    Use Absans font

--- src/components/Subtitle.tsx
+++ src/components/Subtitle.tsx
@@ -6,7 +6,7 @@ type SubtitleProps = {
 
 export const Subtitle = ({ children }: SubtitleProps) => {
   return (
-    <h2 className="font-inter mt-28 text-center text-xl font-normal text-blue-200">
+    <h2 className="font-text mt-28 text-center text-xl font-normal text-blue-200">
       {children}
     </h2>
   );
